a05001d5a9a76124c3fafdcabf12a1007370b770
Adrián <adxrzn@gmail.com>
2026-04-13 21:07:20 +0200

    Arreglado Ejercicio 2

--- server/src/config/env.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-if (!process.env.PORT) {
-  throw new Error('El puerto no está definido en el archivo .env');
-}
-
-module.exports = {
-  PORT: process.env.PORT
-};
--- server/src/routes/task.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const taskController = require('../controllers/task.controller');
-
-router.get('/', taskController.getTasks);
-router.post('/', taskController.createTask);
-
-module.exports = router;
